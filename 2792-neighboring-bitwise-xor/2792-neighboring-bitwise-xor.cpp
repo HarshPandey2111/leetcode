9f3d10a790cb73cf68090aceaab4c868cad575a9
HarshPandey2111 <106433638+HarshPandey2111@users.noreply.github.com>
2025-01-18 11:46:29 +0530

    Time: 0 ms (100%), Space: 263.9 MB (50.63%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -0,0 +1,9 @@
+
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int sum = accumulate(derived.begin(), derived.end(), 0);
+        return sum % 2 == 0;
+    }
+};
